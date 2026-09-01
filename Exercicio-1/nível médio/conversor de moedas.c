3d0447cc8aef6f4d6d71c9c9a2b6f109e4569f29
LuanDlsa <deividluan805@gmail.com>
2026-09-01 00:24:41 -0300

    Create conversor de moedas.c

---
+++ Exercicio-1/nível médio/conversor de moedas.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+float valor1;
+float valor2;
+float resultado;
+
+int main(){ 
+   printf("===Conversor de Real para Dolar===\n\n");
+    printf("Valor em Real\n");
+      scanf(" %f", &valor1);
+    
+    
+    printf("cotacao do dolar:\n");
+      scanf(" %f", &valor2);
+
+resultado = valor1 / valor2;
+ 
+    printf("O valor R$ %.2f com a cotacao do dolar R$ %.2f ,  resulta em US$ %.2f", valor1, valor2, resultado);
+
+return 0;  }
