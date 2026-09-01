1541eea9f6cb286db94e89f36db47959517e0f21
LuanDlsa <deividluan805@gmail.com>
2026-09-01 18:37:53 -0300

    Create Dados Do Aluno.c

---
+++ Exercício 1/1-Nivel Fácil/Dados Do Aluno.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h> // usar funções que estão declaradas na biblioteca padrão stdlib.h
+int main(){
+     printf("--Faculdade UNIALFA--\n\n"); // printf é usado para imprimir uma mensagem na tela
+     printf("Nome Completo: \n"); // (/n) cada vez que é adicionado pula uma linha
+     printf("DEIVID LUAN SILVA DE ASSIS \n\n");
+     printf("Curso: \n");
+     printf("Analise e Desenvolvimento de Sistemas \n\n");
+     printf("Turma: \n");
+     printf(" 335D \n\n");
+     printf("Ano de ingresso: \n");
+     printf(" 2026\n\n");
+
+         system("pause"); // comando para manter a aba da execução do codigo aberta até pressionar qualquer tecla
+
+      return 0;
+}
