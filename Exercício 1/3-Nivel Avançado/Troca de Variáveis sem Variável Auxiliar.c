c3138e71420ad75c812013454b6b6c4d5ba1633f
LuanDlsa <deividluan805@gmail.com>
2026-09-01 19:28:22 -0300

    Update Troca de Variáveis sem Variável Auxiliar.c

--- Exercício 1/3-Nivel Avançado/Troca de Variáveis sem Variável Auxiliar.c
+++ Exercício 1/3-Nivel Avançado/Troca de Variáveis sem Variável Auxiliar.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+   int X;
+   int Y;
 
+
+int main() {
+      printf("++++++++++Troca de Variaveis sem Variavel Auxiliar++++++++++\n\n");
+
+ printf("digite qualquer numero inteiro para o valor de X\n");
+    scanf(" %d", &X);
+
+printf("\ndigite qualquer numero inteiro para o valor de Y\n");
+    scanf(" %d", &Y);     
+    
+
+    printf("\nos valores digitados foram: X = %d e Y = %d\n\n\n", X, Y);
+
+     X = X + Y;
+     Y = X - Y;
+     X = X - Y;
+     
+     printf("Trocando os valores resulta em: X = %d e Y = %d\n\n\n", X, Y);
+
+     system("pause");
+ return 0;
+}
+ 
